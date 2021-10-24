fe3d526c0ccb4eeaec28b432538d30f27eb334cb
daoraCode <severin.mboukou@gmail.com>
2021-10-24 20:55:49 +0200

    es6 template done

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+// push sans push
 let fruits = ['Apple', 'Orange', 'Banana'];
-// fruits = [...fruits, 'Raspberry'];
-console.log(fruits);
+fruits = [...fruits, 'Raspberry'];
 
+// Unshift sans unshift
 fruits = ['Apple', 'Orange', 'Banana'];
 fruits = ['Raspberry', ...fruits];
-console.log(fruits);
 
+
+// Array concatenation
 const arr1 = ['A', 'B', 'C'];
 const arr2 = ['X', 'Y', 'Z'];
 
 const arr3 = [...arr1, ...arr2];
-console.log(arr3);
 
+// Objects
 const user = {
   age: 10,
   eyes: 'vert',
@@ -29,4 +31,15 @@ const bobby = {
   ...user,
   firstName: 'bobby',
 };
-console.log(bobby);
+
+// forEach
+let numbers = [1, 2, 3, 4, 5, 6];
+numbers.forEach(num => console.log(num));
+
+// filter
+let filteredNumbers = numbers.filter(num => num % 2 !== 0);
+console.log(filteredNumbers);
+
+// map
+let squaredNumbers = numbers.map(num => num ** 2);
+console.log(squaredNumbers);
